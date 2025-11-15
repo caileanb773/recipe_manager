aa989eecc0f76fb895d72042a2a48d34c7e7b890
caileanb <cailean.bernard@gmail.com>
2025-11-15 07:09:13 -0500

    notif changes

--- src/view/NotificationButton.java
+++ src/view/NotificationButton.java
@@ -1,8 +1,10 @@
 package view;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
+
 import definitions.Notification;
 
 /*
@@ -20,12 +22,14 @@ public class NotificationButton extends JButton {
 	private JLabel sender;
 	private JLabel notes;
 	private JButton expand;
-	
+	//private ImageIcon divider;	// separates each visual element of the notif btn 
+	private final JLabel divider = new JLabel("|"); // might look okay as just "|"
 	
 	public NotificationButton(Notification notification) {
 		new JButton();
 		this.notification = notification;
 		setFocusable(true);
+		boolean displayWithNotes = false;
 		
 		// Store the values locally for now, maybe try without this if it is slow
 		String notifType = notification.getNotificationType().name();
@@ -33,12 +37,39 @@ public class NotificationButton extends JButton {
 		String notifSenderName = notification.getSender().getName();
 		String notifNotes = notification.getNotes();
 		
+		if (notifNotes != null && !notifNotes.isEmpty()) {
+			displayWithNotes = true;
+		}
+		
+		// Initialize button components with notification properties
 		checkBx = new JCheckBox();
 		type = new JLabel(notifType);
 		rcpName = new JLabel(notifRcpName);
 		sender = new JLabel(notifSenderName);
-		notes = new JLabel(notifNotes);			// Make sure to truncate this
+		
+		if (displayWithNotes) {
+			notes = new JLabel(notifNotes);			// Make sure to truncate this
+		}
+		
 		expand = new JButton("X");
+		
+		// Lay out button components
+		add(checkBx);
+		add(divider);
+		add(type);
+		add(divider);
+		add(rcpName);
+		add(divider);
+		add(sender);
+		add(divider);
+		
+		// probably will need to add some kind of placeholder if we're not displaying notes
+		if (displayWithNotes) {
+			add(notes);
+			add(divider);
+		}
+		
+		add(expand);
 	}
 	
 }
