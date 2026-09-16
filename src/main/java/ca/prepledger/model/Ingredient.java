47ca2557df26bd88dae67e5bee16e3ff6fb70eba
caileanb <cailean.bernard@gmail.com>
2026-09-16 08:21:50 -0400

    added notes field to ingredient object

--- src/main/java/ca/prepledger/model/Ingredient.java
+++ src/main/java/ca/prepledger/model/Ingredient.java
@@ -12,57 +12,80 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Ingredient {
-	
+
 	private Fraction amount;
 	private String unit;
 	private String name;
+	private String notes;
 
-	
+
 	public Ingredient() {}
-	
-	public Ingredient(Fraction amount, String u, String n) {
+
+	public Ingredient(
+			Fraction amount,
+			String unit,
+			String name,
+			String notes) {
 		this.amount = amount;
-		name = n;
-		unit = u;
+		this.unit = unit;
+		this.name = name;
+		this.notes = notes;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getUnit() {
 		return unit;
 	}
-	
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
 	@JsonIgnore
 	public Fraction getAmountFraction() {
 		return amount;
 	}
-	
+
 	@JsonProperty("amount")
 	public String getAmount() {
 		return amount.toString();
 	}
-	
+
 	@Override
 	public String toString() {
-		return amount.toString() + " " + unit.toString().toLowerCase() + " " + name;
+		return amount.toString() + " " + unit.toString().toLowerCase() + " " + name + " " + notes;
 	}
-	
+
 	@Override
 	public boolean equals(Object o) {
-	    if (this == o) return true;
-	    if (!(o instanceof Ingredient)) return false;
+		if (this == o) return true;
+		if (!(o instanceof Ingredient)) return false;
 
-	    Ingredient other = (Ingredient) o;
-	    return Objects.equals(amount, other.amount) &&
-	           unit == other.unit &&
-	           Objects.equals(name, other.name);
+		Ingredient other = (Ingredient) o;
+		return Objects.equals(amount, other.amount) &&
+				Objects.equals(unit, other.unit) &&
+				Objects.equals(name, other.name) &&
+				Objects.equals(notes, other.notes);
 	}
 
 	@Override
 	public int hashCode() {
-	    return Objects.hash(amount, unit, name);
+		return Objects.hash(amount, unit, name, notes);
 	}
 
 }
