5c931cfd48a39ed59bfe4fb59c8681a2d56c5bd5
caileanb <cailean.bernard@gmail.com>
2026-09-16 06:34:25 -0400

    playing w/ app dimensions

--- src/main/java/ca/prepledger/app/Main.java
+++ src/main/java/ca/prepledger/app/Main.java
@@ -29,7 +29,7 @@ public class Main extends Application {
 		
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/AppShell.fxml"));
 		Parent root = loader.load();
-		Scene scene = new Scene(root, 1280, 800);
+		Scene scene = new Scene(root, 900, 900);
 		
 	    scene.getStylesheets().add(
 	            getClass().getResource("/css/application.css").toExternalForm()
