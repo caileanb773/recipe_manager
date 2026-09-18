3021f24f79b3ca73c3cc45cf59c697f9199915ca
caileanb <cailean.bernard@gmail.com>
2026-09-17 23:51:59 -0400

    recipeservice methods

--- src/main/java/ca/prepledger/service/RecipeService.java
+++ src/main/java/ca/prepledger/service/RecipeService.java
@@ -1,5 +1,23 @@
 package ca.prepledger.service;
 
+import java.util.ArrayList;
+
+import ca.prepledger.model.Recipe;
+
 public class RecipeService {
+	
+	// private RecipeRepository repository;
+	
+	// XXX to be removed
+	private ArrayList<Recipe> dummyRecipeList;
+	
+	
+	// Methods
+	
+	// XXX
+	public ArrayList<Recipe> getAllRecipes() {
+		System.out.println("getAllRecipes() here");
+		return dummyRecipeList;
+	}
 
 }
