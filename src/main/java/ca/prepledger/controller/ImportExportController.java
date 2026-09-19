3b79dfbb87d18298de019fcbaed7011c1ea86f46
caileanb <cailean.bernard@gmail.com>
2026-09-19 19:37:39 -0400

    gave importexportcontroller dependency injection and ref to required services

--- src/main/java/ca/prepledger/controller/ImportExportController.java
+++ src/main/java/ca/prepledger/controller/ImportExportController.java
@@ -1,5 +1,7 @@
 package ca.prepledger.controller;
 
+import ca.prepledger.service.ImportExportService;
+import ca.prepledger.service.RecipeService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
@@ -8,10 +10,18 @@ public class ImportExportController {
 	@FXML
 	private Button exportBtn;
 	
+	private RecipeService recipeService;
+	
+	private ImportExportService impExpService = new ImportExportService();
+	
 	
 	@FXML
 	public void onExportBtnClicked() {
 		System.out.println("click!");
+	}
+	
+	public void setRecipeService(RecipeService recipeService) {
+		this.recipeService = recipeService;
 	}
 
 }
