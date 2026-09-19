19474c18e2512b7bf0584b0d60bf416fc5272f72
caileanb <cailean.bernard@gmail.com>
2026-09-19 19:36:22 -0400

    new class: importexportservice

---
+++ src/main/java/ca/prepledger/service/ImportExportService.java
@@ -0,0 +1,19 @@
+package ca.prepledger.service;
+
+import java.util.List;
+
+import ca.prepledger.model.Recipe;
+
+public class ImportExportService {
+	
+	
+	public List<Recipe> importRecipes() {
+		// TODO complete
+		return null;
+	}
+	
+	public void exportRecipes(List<Recipe> recipes) {
+		
+	}
+
+}
