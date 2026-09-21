540a2f81f5954aa04aeb8541b0b61d455a5e2fe2
caileanb <cailean.bernard@gmail.com>
2026-09-21 10:33:40 -0400

    basic functionality/linking of elements for recipeview.fxml and controller

--- src/main/java/ca/prepledger/controller/RecipeViewController.java
+++ src/main/java/ca/prepledger/controller/RecipeViewController.java
@@ -1,4 +1,65 @@
+package ca.prepledger.controller;
+
+import ca.prepledger.model.Recipe;
+import ca.prepledger.navigation.NavigationHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 
 public class RecipeViewController {
+	
+	@FXML
+	private Button navBackButton;
+	
+	@FXML
+	private Label recipeTitleHeader;
+	
+	@FXML
+	private Button editButton;
+	
+	@FXML
+	private Button removeButton;
+	
+	@FXML
+	private HBox tagsHBox;
+	
+	private Recipe recipe;
+	
+	private NavigationHandler navigationHandler;
+	
+	
+	public void setNavigationHandler(AppShellController appShellController) {
+		navigationHandler = appShellController;
+	}
+
+	public void setRecipeToView(Recipe recipe) {
+		this.recipe = recipe;
+		
+		// Set the title header
+		recipeTitleHeader.setText(recipe.getTitle());
+		
+		// Add tags
+		for (String tag : recipe.getTags()) {
+			Label tagLabel = new Label(tag);
+			tagLabel.getStyleClass().add("header-3");
+			tagsHBox.getChildren().add(tagLabel);			
+		}
+	}
+	
+	@FXML
+	private void onNavBackButtonClicked() {
+		
+	}
+	
+	@FXML
+	private void onEditButtonClicked() {
+		
+	}
+	
+	@FXML
+	private void onRemoveRecipeButtonClicked() {
+		
+	}
 
 }
