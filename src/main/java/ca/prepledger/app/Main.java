f70c1e6bd21a10d1c25719bd46c7b5d6669bc564
caileanb <cailean.bernard@gmail.com>
2026-09-26 09:24:01 -0400

    added logging to main

--- src/main/java/ca/prepledger/app/Main.java
+++ src/main/java/ca/prepledger/app/Main.java
@@ -62,7 +62,7 @@ public class Main extends Application {
 		try {
 			cfgManager.save(appCfg);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			logger.warn("IOException caught during ConfigManager.save() during Main.stop().");
 			e.printStackTrace();
 		}
 	}
